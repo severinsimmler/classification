365c95a599b67deccc25850dbbd64749b1ee7471
severinsimmler <severin.simmler@posteo.de>
2019-08-22 16:31:52 +0200

    feat: notebook

--- notebooks/classification/test.py
+++
@@ -1,7 +0,0 @@
-scores = list()
-
-for corpus in {"wikipedia", "dramen", "romane", "wikipedia", "zeitung"}:
-    score = dict(classification.classic_pipeline(corpus, downsample=False))
-    score = pd.Series(score)
-    score.name = corpus
-    scores.append(score)
