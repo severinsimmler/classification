d062fe76939ebd313e3ab70587a65368a15e3e46
severinsimmler <severin.simmler@posteo.de>
2019-09-04 00:04:36 +0200

    feat: script

--- feature-based/optimize.py
+++ feature-based/optimize.py
@@ -1,12 +1,7 @@
 from pathlib import Path
 
 from flair.datasets import ClassificationCorpus
-from flair.embeddings import (
-    BertEmbeddings,
-    XLNetEmbeddings,
-    XLMEmbeddings,
-    DocumentRNNEmbeddings,
-)
+from flair.embeddings import BertEmbeddings, DocumentRNNEmbeddings
 from flair.models import TextClassifier
 from flair.trainers import ModelTrainer
 from hyperopt import hp
@@ -19,43 +14,39 @@ from flair.hyperparameter.param_selection import (
 
 if __name__ == "__main__":
     data_folder = Path("classification", "data", "downsampled", "flair")
+    for c in ["dramen", "romane", "zeitung", "wikipedia"]:
+        test_file = f"{c}-downsampled-test-flair.txt"
+        dev_file = f"{c}-downsampled-val-flair.txt"
+        train_file = f"{c}-downsampled-train-flair.txt"
 
-    for model, embedding in [
-        ("bert", BertEmbeddings("bert-base-german-cased")),
-        ("xlm", XLMEmbeddings("xlm-mlm-ende-1024")),
-    ]:
-        for c in ["dramen", "romane", "zeitung", "wikipedia"]:
-            test_file = f"{c}-downsampled-test-flair.txt"
-            dev_file = f"{c}-downsampled-val-flair.txt"
-            train_file = f"{c}-downsampled-train-flair.txt"
+        corpus = ClassificationCorpus(
+            data_folder, test_file=test_file, dev_file=dev_file, train_file=train_file
+        )
 
-            corpus = ClassificationCorpus(
-                data_folder,
-                test_file=test_file,
-                dev_file=dev_file,
-                train_file=train_file,
-            )
+        label_dict = corpus.make_label_dictionary()
 
-            label_dict = corpus.make_label_dictionary()
+        search_space = SearchSpace()
+        search_space.add(
+            Parameter.EMBEDDINGS,
+            hp.choice,
+            options=[[BertEmbeddings("bert-base-german-cased")]],
+        )
+        search_space.add(Parameter.HIDDEN_SIZE, hp.choice, options=[32, 64, 128])
+        search_space.add(Parameter.RNN_LAYERS, hp.choice, options=[1, 2])
+        search_space.add(Parameter.DROPOUT, hp.uniform, low=0.0, high=0.5)
+        search_space.add(
+            Parameter.LEARNING_RATE, hp.choice, options=[0.05, 0.1, 0.15, 0.2]
+        )
+        search_space.add(Parameter.MINI_BATCH_SIZE, hp.choice, options=[8, 16, 32])
 
-            search_space = SearchSpace()
-            search_space.add(Parameter.EMBEDDINGS, hp.choice, options=[[embedding]])
-            search_space.add(Parameter.HIDDEN_SIZE, hp.choice, options=[32, 64, 128])
-            search_space.add(Parameter.RNN_LAYERS, hp.choice, options=[1, 2])
-            search_space.add(Parameter.DROPOUT, hp.uniform, low=0.0, high=0.5)
-            search_space.add(
-                Parameter.LEARNING_RATE, hp.choice, options=[0.05, 0.1, 0.15, 0.2]
-            )
-            search_space.add(Parameter.MINI_BATCH_SIZE, hp.choice, options=[32])
+        param_selector = TextClassifierParamSelector(
+            corpus,
+            False,
+            f"optimization/{c}",
+            "lstm",
+            max_epochs=5,
+            training_runs=1,
+            optimization_value=OptimizationValue.DEV_SCORE,
+        )
 
-            param_selector = TextClassifierParamSelector(
-                corpus,
-                False,
-                f"optimization/{model}/{c}",
-                "lstm",
-                max_epochs=5,
-                training_runs=1,
-                optimization_value=OptimizationValue.DEV_SCORE,
-            )
-
-            param_selector.optimize(search_space, max_evals=5)
+        param_selector.optimize(search_space, max_evals=5)
