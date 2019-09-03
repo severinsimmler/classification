f798dcfb632bf63adb3a09342949f5178da8703f
severinsimmler <severin.simmler@posteo.de>
2019-09-04 00:13:24 +0200

    stuff

--- fine-tuned/train-loop.py
+++ fine-tuned/train-loop.py
@@ -58,77 +58,67 @@ def get_batches(train_data):
 
 
 if __name__ == "__main__":
-    for model in ["de_pytt_bertbasecased_lg", "xlm-mlm-ende-1024"]:
-        for corpus in ["dramen", "romane", "zeitung", "wikipedia"]:
-            if model == "de_pytt_bertbasecased_lg" and corpus == "dramen":
-                continue
-            print(model, corpus)
-            dataset = preprocessing.load(corpus, split=True, downsample=True)
-            if model == "de_pytt_bertbasecased_lg":
-                nlp = spacy.load(model)
-            else:
-                nlp = PyTT_Language(pytt_name=model, meta={"lang": "de"})
-                nlp.add_pipe(nlp.create_pipe("sentencizer"))
-                nlp.add_pipe(PyTT_WordPiecer.from_pretrained(nlp.vocab, model))
-                nlp.add_pipe(PyTT_TokenVectorEncoder.from_pretrained(nlp.vocab, model))
-            textcat = nlp.create_pipe(
-                "pytt_textcat", config={"architecture": "softmax_last_hidden"}
-            )
-            for label in LABELS:
-                textcat.add_label(label)
+    for corpus in ["dramen", "romane", "zeitung", "wikipedia"]:
+        dataset = preprocessing.load(corpus, split=True, downsample=True)
+        nlp = spacy.load("de_pytt_bertbasecased_lg")
+        textcat = nlp.create_pipe(
+            "pytt_textcat", config={"architecture": "softmax_last_hidden"}
+        )
+        for label in LABELS:
+            textcat.add_label(label)
 
-            X = dataset["train"]["text"]
-            y = dataset["train"]["class"]
+        X = dataset["train"]["text"]
+        y = dataset["train"]["class"]
 
-            train_data = list(
-                zip(
-                    X,
-                    [
-                        {
-                            "cats": {
-                                "0": 1 if label == 0 else 0,
-                                "1": 1 if label == 1 else 0,
-                                "2": 1 if label == 2 else 0,
-                            }
+        train_data = list(
+            zip(
+                X,
+                [
+                    {
+                        "cats": {
+                            "0": 1 if label == 0 else 0,
+                            "1": 1 if label == 1 else 0,
+                            "2": 1 if label == 2 else 0,
                         }
-                        for label in y
-                    ],
+                    }
+                    for label in y
+                ],
+            )
+        )
+
+        nlp.add_pipe(textcat, last=True)
+
+        optimizer = nlp.resume_training()
+        optimizer.alpha = 0.001
+        optimizer.pytt_weight_decay = 0.005
+        optimizer.L2 = 0.0
+        learn_rates = cyclic_triangular_rate(
+            LEARN_RATE / 3, LEARN_RATE * 3, 2 * len(train_data) // MAX_BATCH_SIZE
+        )
+        losses = collections.Counter()
+
+        SCORES = {"TEST": list(), "DEV": list()}
+        for epoch in range(MAX_EPOCHS):
+            logging.error(f"Epoch #{epoch + 1}")
+            random.shuffle(train_data)
+            batches = get_batches(train_data)
+            for batch in batches:
+                optimizer.pytt_lr = next(learn_rates)
+                texts, annotations = zip(*batch)
+                dropout = next(DROPOUT)
+                nlp.update(
+                    texts, annotations, sgd=optimizer, drop=dropout, losses=losses
                 )
-            )
 
-            nlp.add_pipe(textcat, last=True)
+            stats = list()
+            for _, row in dataset["test"].iterrows():
+                t = nlp(row["text"])
+                pred = int(max(t.cats.items(), key=operator.itemgetter(1))[0])
+                if pred == row["class"]:
+                    stats.append(1)
+                else:
+                    stats.append(0)
 
-            optimizer = nlp.resume_training()
-            optimizer.alpha = 0.001
-            optimizer.pytt_weight_decay = 0.005
-            optimizer.L2 = 0.0
-            learn_rates = cyclic_triangular_rate(
-                LEARN_RATE / 3, LEARN_RATE * 3, 2 * len(train_data) // MAX_BATCH_SIZE
-            )
-            losses = collections.Counter()
-
-            SCORES = {"TEST": list(), "DEV": list()}
-            for epoch in range(MAX_EPOCHS):
-                logging.error(f"Epoch #{epoch + 1}")
-                random.shuffle(train_data)
-                batches = get_batches(train_data)
-                for batch in batches:
-                    optimizer.pytt_lr = next(learn_rates)
-                    texts, annotations = zip(*batch)
-                    dropout = next(DROPOUT)
-                    nlp.update(
-                        texts, annotations, sgd=optimizer, drop=dropout, losses=losses
-                    )
-
-                stats = list()
-                for _, row in dataset["test"].iterrows():
-                    t = nlp(row["text"])
-                    pred = int(max(t.cats.items(), key=operator.itemgetter(1))[0])
-                    if pred == row["class"]:
-                        stats.append(1)
-                    else:
-                        stats.append(0)
-
-                SCORES["TEST"].append(sum(stats) / len(dataset["test"]))
-                print(max(SCORES["TEST"]))
-            print(SCORES)
+            SCORES["TEST"].append(sum(stats) / len(dataset["test"]))
+            print(max(SCORES["TEST"]))
+        print(SCORES)
