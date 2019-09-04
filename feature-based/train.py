aaec595ec31d0429b3c960a395b3f3aa20759839
severinsimmler <severin.simmler@posteo.de>
2019-09-04 16:25:51 +0200

    feat: hyperparameter values

--- feature-based/train.py
+++ feature-based/train.py
@@ -10,29 +10,28 @@ if __name__ == "__main__":
     data_folder = Path("..", "classification", "data", "downsampled", "flair")
     for c in ["dramen", "romane", "zeitung", "wikipedia"]:
         if c == "dramen":
-            dropout = 0.4949748569324362
-            hidden_size = 64
-            learning_rate = 0.05
-            mini_batch_size = 32
-            rnn_layers = 2
-            hidden_size = 64
-        elif c == "romane":
-            dropout = 0.3874021064249358
+            dropout = 0.2271030168362283
             hidden_size = 128
-            learning_rate = 0.1
-            mini_batch_size = 32
+            learning_rate = 0.05
+            mini_batch_size = 16
+            rnn_layers = 2
+        elif c == "romane":
+            dropout = 0.41990906860718336
+            hidden_size = 128
+            learning_rate = 0.15
+            mini_batch_size = 16
             rnn_layers = 2
         elif c == "zeitung":
-            dropout = 0.08010071245223199
-            hidden_size = 32
-            learning_rate = 0.05
-            mini_batch_size = 32
-            rnn_layers = 1
-        elif c == "wikipedia":
-            dropout = 0.3414306905512738
+            dropout = 0.4781300177858385
             hidden_size = 128
-            learning_rate = 0.05
+            learning_rate = 0.15
             mini_batch_size = 32
+            rnn_layers = 2
+        elif c == "wikipedia":
+            dropout = 0.16967065030214346
+            hidden_size = 64
+            learning_rate = 0.05
+            mini_batch_size = 16
             rnn_layers = 2
 
         test_file = f"{c}-downsampled-test-flair.txt"
@@ -64,6 +63,6 @@ if __name__ == "__main__":
             f"models/{c}",
             learning_rate=learning_rate,
             mini_batch_size=mini_batch_size,
-            max_epochs=20,
+            max_epochs=50,
         )
 
